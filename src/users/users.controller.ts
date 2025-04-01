00e9cd3daef92587e6c6ad9fff742e94115729b2
Kaung Satt Hein <kaungsatthein@abank.com.mm>
2025-04-01 14:41:59 +0630

    {Validation and Pipes}

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,21 +6,44 @@ import {
   Query,
   Body,
   Headers,
+  ParseIntPipe,
+  DefaultValuePipe,
+  ValidationPipe,
+  Patch,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id{/:optional}')
-  public getAllUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log('params', id);
+  @Get('/{:id}')
+  public getAllUsers(
+    @Param() getUsersParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log('params', typeof getUsersParamDto);
+    console.log('getUsersParamDto', getUsersParamDto);
+    console.log('type of limit', typeof limit);
+    console.log('type of page', typeof page);
     console.log('limit', limit);
+    console.log('page', page);
     return 'get all users';
   }
 
   @Post()
-  public createUser(@Body() request: any, @Headers() headers: any) {
+  public createUser(
+    @Headers() headers: any,
+    @Body() createUserDto: CreateUserDto,
+  ) {
     console.log('headers', headers);
-    console.log('request', request);
+    console.log('request', createUserDto);
     return 'create a user';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
